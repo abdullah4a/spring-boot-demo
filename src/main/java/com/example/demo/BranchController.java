7a3b5e0a84f57b045b804a32e0130685dffe4ed1
wa_ab <abdullahkhan4a@gmail.com>
2022-01-01 19:14:34 +0500

    Branch GetbyId Fixed

--- src/main/java/com/example/demo/BranchController.java
+++ src/main/java/com/example/demo/BranchController.java
@@ -10,12 +10,12 @@ import java.util.List;
 @RequestMapping("/branch")
 public class BranchController {
     @PostMapping("/")
-    public BranchModel post(@RequestBody BranchModel model){
+    public BranchModel post(@RequestBody BranchModel model) {
         return model;
     }
 
     @GetMapping("/list")
-    public List<BranchModel> getAll(){
+    public List<BranchModel> getAll() {
         return Collections.singletonList(new BranchModel(1l,
                 "Test",
                 "Model",
@@ -33,9 +33,30 @@ public class BranchController {
                         "ABC"
                 )));
     }
+
     @DeleteMapping("/{id}")
     public Boolean deleteById(@PathVariable("id") Long webId) {
         return true;
+    }
+
+    @GetMapping("/{id}")
+    public BranchModel getById(@PathVariable("id") Long webId) {
+        return new BranchModel(1l,
+                "Test",
+                "Model",
+                "3213121",
+                "+6465421",
+                "TEST BRANCH HELD HERE",
+                "TEST @ TEST.COM",
+                "testBranch.com",
+                "+9265146",
+                true,
+                new AddressModel(2980l,
+                        "street",
+                        "USA",
+                        "California",
+                        "ABC"
+                ));
     }
 
     @PutMapping("/{id}")
