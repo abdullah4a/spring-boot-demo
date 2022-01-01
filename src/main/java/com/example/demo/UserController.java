d66bfc91402fa586ac7f880a018d6f315ec0919b
wa_ab <abdullahkhan4a@gmail.com>
2022-01-01 16:03:59 +0500

    User List Fixed

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -35,7 +35,6 @@ public class UserController {
 
     @PostMapping("/")
     public UserModel post(@RequestBody UserModel model){
-//        userModel(model.setWebId());
         return model;
     }
 
