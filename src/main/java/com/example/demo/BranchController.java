8c2059305793a659bbac85ea4e2824e66e1a2eb5
abdullah4a <abdullahkhan4a@gmail.com>
2022-01-03 13:40:11 +0500

    Branch's Exception Handling Fixed

--- src/main/java/com/example/demo/BranchController.java
+++ src/main/java/com/example/demo/BranchController.java
@@ -36,31 +36,57 @@ public class BranchController {
 
     @DeleteMapping("/{id}")
     public Boolean deleteById(@PathVariable("id") Long webId) {
-        return true;
+        BranchModel model=null;
+        try {
+            if (webId == model.getWebId()) {
+                return true;
+            }
+        } catch (ExceptionHandler exc) {
+            throw new ExceptionHandler("Branch Id Not Found", exc);
+        }
+        return false;
     }
 
     @GetMapping("/{id}")
     public BranchModel getById(@PathVariable("id") Long webId) {
-        return new BranchModel(1l,
-                "Test",
-                "Model",
-                "3213121",
-                "+6465421",
-                "TEST BRANCH HELD HERE",
-                "TEST @ TEST.COM",
-                "testBranch.com",
-                "+9265146",
-                true,
-                new AddressModel(2980l,
-                        "street",
-                        "USA",
-                        "California",
-                        "ABC"
-                ));
+        BranchModel model=null;
+        try {
+            if (webId == model.getWebId()) {
+                return new BranchModel(1l,
+                        "Test",
+                        "Model",
+                        "3213121",
+                        "+6465421",
+                        "TEST BRANCH HELD HERE",
+                        "TEST @ TEST.COM",
+                        "testBranch.com",
+                        "+9265146",
+                        true,
+                        new AddressModel(2980l,
+                                "street",
+                                "USA",
+                                "California",
+                                "ABC"
+                        ));
+            }
+        } catch (ExceptionHandler exc) {
+            throw new ExceptionHandler("Branch Id Not Found", exc);
+        }
+        return null;
     }
 
     @PutMapping("/{id}")
     public BranchModel update(@PathVariable("id") Long webId, @RequestBody BranchModel model) {
-        return model;
+        BranchModel branchModel=null;
+        try {
+            if (webId == model.getWebId()) {
+//                TODO updating of branch left
+                return model;
+
+            }
+        } catch (ExceptionHandler exc) {
+            throw new ExceptionHandler("Branch Id Not Found", exc);
+        }
+        return null;
     }
 }
