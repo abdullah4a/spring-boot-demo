af03779b3bde583cf16da79a15aad519a67381f5
abdullah4a <abdullahkhan4a@gmail.com>
2022-01-03 12:04:31 +0500

    Exception Handling Fixed

--- src/main/java/com/example/demo/CategoryController.java
+++ src/main/java/com/example/demo/CategoryController.java
@@ -23,20 +23,16 @@ public class CategoryController {
     //Trying New Methods
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public CategoryModel getById(@PathVariable("id") Long webId) {
-        CategoryModel model = null;
-       try
-       {
-           if (webId==model.getWebId()){
-               return new CategoryModel(1l,
-                       "3213",
-                       "TEST",
-                       "MODEL");
-           }
-       }catch (ExceptionHandler exc) {
-           throw new ExceptionHandler(
-                   "Category Not Found", exc);
-       }
-       return null;
+        CategoryModel model=null;
+        try {
+            if (webId == model.getWebId()) {
+                return new CategoryModel(model.getWebId(),
+                        "3654", "TEST CATEGORY", "TEST SUBCATEGORY");
+            }
+        } catch (ExceptionHandler exc) {
+            throw new ExceptionHandler("Category Id Not Found", exc);
+        }
+        return null;
     }
 
     @PostMapping("/")
@@ -46,11 +42,28 @@ public class CategoryController {
 
     @DeleteMapping("/{id}")
     public Boolean deleteById(@PathVariable("id") Long webId) {
-        return true;
+        CategoryModel model = null;
+        try {
+            if (webId == model.getWebId()) {
+                return true;
+            }
+        } catch (ExceptionHandler exc) {
+            throw new ExceptionHandler("Category Id Not Found", exc);
+        }
+        return false;
     }
 
     @PutMapping("/{id}")
     public CategoryModel update(@PathVariable("id") Long webId, @RequestBody CategoryModel model) {
-        return model;
+        CategoryModel categoryModel = null;
+        try {
+            if (webId == categoryModel.getWebId()) {
+//                TODO Updating the Model remaining
+                return model;
+            }
+        } catch (ExceptionHandler exc) {
+            throw new ExceptionHandler("Category Id Not Found", exc);
+        }
+        return null;
     }
 }
